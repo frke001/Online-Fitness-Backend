fdabb8ac9f046a16d71d7f09446a4ce40b6e09ff
NDj01 <nemanjadj102@gmail.com>
2024-01-03 23:39:28 +0100

    Updated Category Entities

--- src/main/java/org/unibl/etf/fitness/models/entities/CategoryAttributeEntity.java
+++ src/main/java/org/unibl/etf/fitness/models/entities/CategoryAttributeEntity.java
@@ -1,4 +1,23 @@
 package org.unibl.etf.fitness.models.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "category_attribute")
 public class CategoryAttributeEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    protected Long id;
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 50)
+    protected String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id") // spajamo kolonu category_id u ovoj klasi i id u drugoj tabeli
+    private CategoryEntity category;
 }
--- src/main/java/org/unibl/etf/fitness/models/entities/CategoryEntity.java
+++ src/main/java/org/unibl/etf/fitness/models/entities/CategoryEntity.java
@@ -1,4 +1,25 @@
 package org.unibl.etf.fitness.models.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "category")
 public class CategoryEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 50)
+    protected String name;
+
+    @OneToMany(mappedBy = "category")
+    List<CategoryAttributeEntity> attributes;
+
 }
